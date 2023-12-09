a7f46239c016b04e8ab48c8c2446884019f80a0b
Andrés García <playsharp59@gmail.com>
2023-12-09 20:27:25 +0100

    init

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const contentUrls = {
+  2: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149808/noe/2_qa2lvs.mov',
+  3: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149806/noe/3_ku8cp0.mov',
+  5: 'https://res.cloudinary.com/andresgarcia/image/upload/v1702149804/noe/5_c220z3.jpg',
+  7: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149805/noe/7_gko6sg.mp4',
+  9: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149805/noe/9_yp5l8r.mp4',
+  13: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149805/noe/13_us9koj.mp4',
+  15: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149805/noe/15_gwhsnx.mp4',
+  17: 'https://res.cloudinary.com/andresgarcia/image/upload/v1702149806/noe/17_jasjep.jpg',
+  20: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149807/noe/20_swi3fm.mp4',
+  26: 'https://res.cloudinary.com/andresgarcia/image/upload/v1702149807/noe/26_gyo5rr.jpg'
+}
