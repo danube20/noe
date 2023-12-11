509f6eb37807921f503e9e7b9a525b4ef4470cfd
Andrés Felipe <75199307+danube20@users.noreply.github.com>
2023-12-11 14:25:32 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,3 +10,31 @@ const contentUrls = {
   20: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149807/noe/20_swi3fm.mp4',
   26: 'https://res.cloudinary.com/andresgarcia/image/upload/v1702149807/noe/26_gyo5rr.jpg'
 }
+
+const specialDays = Object.keys(contentUrls)
+const currentDate = new Date()
+const currentDay = currentDate.getDate()
+const isSpecialDay = specialDays.includes(currentDay.toString())
+const currentContent = contentUrls[currentDay]
+const contentExtension = currentContent?.split('.')[1]
+const defaultText = document.getElementById('text')
+const videoTag = document.getElementById('videos')
+const videoSource = document.getElementById('videoSource')
+const image = document.getElementById('images')
+
+if (isSpecialDay) {
+  defaultText.style.display = 'none'
+}
+
+if (['mov', 'mp4'].includes(contentExtension)) {
+  image.style.display = 'none'
+  videoTag.setAttribute('src', currentContent)
+} else if (contentExtension === 'jpg') {
+  videoTag.style.display = 'none'
+  videoSource.style.display = 'none'
+  image.setAttribute('src', currentContent)
+} else {
+  image.style.display = 'none'
+  videoTag.style.display = 'none'
+  videoSource.style.display = 'none'
+}
