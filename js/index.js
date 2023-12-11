903b724ec67c37a5eedf0b18e2c8f75b6970ccc9
Andrés Felipe <75199307+danube20@users.noreply.github.com>
2023-12-11 14:36:34 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 const contentUrls = {
-  2: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149808/noe/2_qa2lvs.mov',
-  3: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149806/noe/3_ku8cp0.mov',
-  5: 'https://res.cloudinary.com/andresgarcia/image/upload/v1702149804/noe/5_c220z3.jpg',
-  7: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149805/noe/7_gko6sg.mp4',
-  9: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149805/noe/9_yp5l8r.mp4',
-  13: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149805/noe/13_us9koj.mp4',
-  15: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149805/noe/15_gwhsnx.mp4',
-  17: 'https://res.cloudinary.com/andresgarcia/image/upload/v1702149806/noe/17_jasjep.jpg',
-  20: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149807/noe/20_swi3fm.mp4',
-  26: 'https://res.cloudinary.com/andresgarcia/image/upload/v1702149807/noe/26_gyo5rr.jpg'
+  11: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149808/noe/2_qa2lvs.mov',
+  12: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149806/noe/3_ku8cp0.mov',
+  14: 'https://res.cloudinary.com/andresgarcia/image/upload/v1702149804/noe/5_c220z3.jpg',
+  16: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149805/noe/7_gko6sg.mp4',
+  18: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149805/noe/9_yp5l8r.mp4',
+  22: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149805/noe/13_us9koj.mp4',
+  24: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149805/noe/15_gwhsnx.mp4',
+  26: 'https://res.cloudinary.com/andresgarcia/image/upload/v1702149806/noe/17_jasjep.jpg',
+  29: 'https://res.cloudinary.com/andresgarcia/video/upload/v1702149807/noe/20_swi3fm.mp4',
+  4: 'https://res.cloudinary.com/andresgarcia/image/upload/v1702149807/noe/26_gyo5rr.jpg'
 }
 
 const specialDays = Object.keys(contentUrls)
